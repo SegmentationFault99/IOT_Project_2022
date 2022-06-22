7e03c51d351b14b6b7b1c1468fadc6b17279d525
SegmentationFault99 <62462121+SegmentationFault99@users.noreply.github.com>
2022-06-23 01:35:33 +0200

    Add files via upload

---
+++ Gardens/Garden01.js
@@ -0,0 +1,38 @@
+var mosca = require('mosca');
+
+var settings = {
+  port: 1883
+};
+
+// here we start mosca
+var server = new mosca.Server(settings);
+// fired when the mqtt server is ready
+server.on('ready', function() {
+  console.log(Date() + ' Mosca server started');
+});
+
+// fired when a client is connected
+server.on('clientConnected', function(client) {
+  console.log('Client Connected: ', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published : ', packet.topic.toString()+' '+packet.payload.toString());  
+});
+
+// fired when a client subscribes to a topic
+server.on('subscribed', function(topic, client) {
+  console.log('Subscribed : ', topic, " from : ", client.id);
+});
+
+// fired when a client unsubscribes to a topic
+server.on('unsubscribed', function(topic, client) {
+  console.log('Unsubscribed : ', topic, " from : ", client.id);
+});
+
+// fired when a client is disconnected
+server.on('clientDisconnected', function(client) {
+  console.log('Client Disconnected : ', client.id);
+});
+
